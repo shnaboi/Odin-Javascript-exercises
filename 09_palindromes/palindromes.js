ae373857cfb315470ff8b03f16f4abb08861a5dd
shnaboi <shane.karner@gmail.com>
2023-01-24 12:48:30 -0500

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let stringOne = str.toLowerCase().replace(/\W|/g, "");
+    let stringTwo = stringOne.split("").reverse().join("");
+    return (stringOne === stringTwo);
 };
+
+// REGEX understanding
+// \W+ matches characters such as comma, underscore, periods
+// /g is global
 
 // Do not edit below this line
 module.exports = palindromes;
